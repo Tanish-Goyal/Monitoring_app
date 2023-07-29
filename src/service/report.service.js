a078f1cd99c04731aa035f95f524782980cc6ba9
tanishgcti0150 <tanish.goyal@coriolis.co.in>
2023-07-29 12:07:46 +0530

    Adding change status function

--- src/service/report.service.js
+++ src/service/report.service.js
@@ -28,22 +28,66 @@ const reportService = {
       },
       {
         $group: {
-          _id: '$hostName', // group by the 'hostName' field
-        },
+          _id: null, // Use null to group all results as a single group
+          hostNames: { $addToSet: '$hostName' }, // Collect unique hostNames in an array
+        }
       },
-      {
-        $project: {
-          _id: 0, // Exclude the '_id' field from the output
-          hostName: '$_id', // Rename the '_id' field to 'hostName'
-        },
-      },
-    ]);
+      // {
+      //   $group: {
+      //     _id: '$hostName', // group by the 'hostName' field
+      //   },
+      // },
+      // {
+      //   $project: {
+      //     _id: 0, // Exclude the '_id' field from the output
+      //     hostName: '$_id', // Rename the '_id' field to 'hostName'
+      //   },
+      // },
+    ]);    
+    return uniqueHostNames[0].hostNames;
   },
 
   getReportsByHostName: async (appId, hostName, page, limit) => {
     const reports = await ReportModel.find({appId:appId,hostName:hostName}).skip((page-1)*limit).limit(limit).exec();
     return reports;
   },
+
+  getReportList1: async (appId, page, limit, hostName) => {
+    
+    if ( hostName == undefined){
+      var reports = await ReportModel.find({appId:appId}).skip((page-1)*limit).limit(limit).exec();
+      console.log("First",reports);
+    }
+    else {
+      var reports = await ReportModel.find({appId:appId,hostName:hostName}).skip((page-1)*limit).limit(limit).exec();
+      console.log("Hostname: ",hostName);
+      console.log(reports);
+    
+    }
+    console.log("-----------------------------------")
+    console.log(reports);
+
+    return reports;
+  },
+  
+  updateReportStatus: async (bundleName, newStatus) => {
+  
+      const updatedReport = await ReportModel.findOneAndUpdate(
+        { 
+          bundleName: bundleName
+        },
+        { 
+          $set: { status: newStatus }
+        },
+        { 
+          new: true
+        }
+      );
+    
+      return updatedReport;
+    } 
+  
+  
 
 };
 
